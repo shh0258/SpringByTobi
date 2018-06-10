50e9fed03d767efefa7dbf907e3e0c7d3a7a164a
한상현 <big0258@naver.com>
2018-06-10 11:06:59 +0900

    p180

--- 180607_p54/src/UserDaoTest.java
+++ 180607_p54/src/UserDaoTest.java
@@ -1,27 +1,42 @@
 import java.sql.SQLException;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDaoTest {
-	public static void main (String[] args) throws ClassNotFoundException, SQLException {
+//	public static void main (String[] args) throws ClassNotFoundException, SQLException {
+//		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+////		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+//		UserDao dao = context.getBean("userDao", UserDao.class);//오브젝트 클래스가 기본으로 리턴되는데, 두번째 인자로 제네릭 메소드방식을 통해 형을 캐스팅해줄수 있음 
+//		User user = new User();
+//		user.setId("124f");
+//		user.setName("asdsad");
+//		user.setPassword("married");
+//		
+//		dao.add(user);
+//		System.out.println(user.getId()+ "등록 성공");
+//		System.out.println(user.getName());
+//		
+//		User user2 = dao.get(user.getId());
+//		System.out.println(user2.getName());
+//		
+//		System.out.println(user2.getId() + "조회 성공");
+//	}
+	@Test
+	public void addAndGet() throws SQLException {
 		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		UserDao dao = context.getBean("userDao", UserDao.class);//오브젝트 클래스가 기본으로 리턴되는데, 두번째 인자로 제네릭 메소드방식을 통해 형을 캐스팅해줄수 있음 
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		User user = new User();
 		user.setId("124f");
 		user.setName("asdsad");
 		user.setPassword("married");
-		
 		dao.add(user);
-		System.out.println(user.getId()+ "등록 성공");
-		System.out.println(user.getName());
-		
 		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
 		
-		System.out.println(user2.getId() + "조회 성공");
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
 		
 	}
 }
